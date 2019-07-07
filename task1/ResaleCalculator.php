41d29a067a3bf219c1934e4e4044d369ab584a96
RomanUrakhov <urahovroman@gmail.com>
2019-07-07 11:05:12 +0500

    Update ResaleCalculator.php

--- task1/ResaleCalculator.php
+++ task1/ResaleCalculator.php
@@ -1,15 +1,14 @@
 <?php
+
 class ResaleCalculator
 {
     public function calculateLowestLoss(array $prices): int
     {
         $minLoss = max($prices);
-        for ($i = 0; $i < count($prices) - 1; $i++)
-        {
-            for ($j = $i + 1; $j < count($prices); $j++)
-            {
-                if (abs($prices[$i] - $prices[$j]) <> 0 and abs($prices[$i] - $prices[$j]) < $minLoss)
-                    $minLoss = abs($prices[$i] - $prices[$j]);
+        for ($i = 0; $i < count($prices) - 1; $i++) {
+            for ($j = $i + 1; $j < count($prices); $j++) {
+                if ($prices[$i] - $prices[$j] > 0 and $prices[$i] - $prices[$j] < $minLoss)
+                    $minLoss = $prices[$i] - $prices[$j];
             }
         }
         return $minLoss;
